d38d8bee846794c015d84a1b59701f26579494cf
shalini-itobuz <shalini@itobuz.com>
2024-02-20 14:32:30 +0530

    latest 1.0

--- script.js
+++ script.js
@@ -26,19 +26,35 @@ function getWeather() {
             temperatureDisplay.innerHTML = `${temperature}°C`;
 
             const datePlace = document.getElementById("datePlace");
-            datePlace.innerHTML = `<div class="day">${currentDate}</div><div class="place"><span><i class="fa-solid fa-location-dot"></i></span>${placeInput}</div>`;
+            const datePlaceDiv = document.createElement("div");
+            const dayDiv = document.createElement("div");
+            const placeDiv = document.createElement("div");
+            const placeSpan = document.createElement("span");
+            const locationIcon = document.createElement("i");
+
+            datePlaceDiv.classList.add("datePlace");
+            dayDiv.classList.add("day");
+            dayDiv.textContent = currentDate;
+            placeDiv.classList.add("place");
+            locationIcon.classList.add("fa-solid", "fa-location-dot");
+            placeSpan.appendChild(locationIcon);
+            placeDiv.appendChild(placeSpan);
+            placeDiv.textContent = placeInput;
+
+            datePlace.innerHTML = "";
+            datePlace.appendChild(dayDiv);
+            datePlace.appendChild(placeDiv);
 
             const section = document.querySelector(".season-section");
             section.classList.remove("day", "rainy", "night");
 
             if (weatherCondition.toLowerCase().includes("sunny")) {
                 section.classList.add("day");
-            } else if (weatherCondition.toLowerCase().includes(["cloud", "Partly cloudy"])) {
+            } else if (
+                weatherCondition.toLowerCase().includes(["cloud", "Partly cloudy"])
+            ) {
                 section.classList.add("night");
-            } else 
-                section.classList.add("rainy");
-            
-          
+            } else section.classList.add("rainy");
         })
         .catch((error) => console.log(error));
 }
